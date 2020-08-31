74ed0071d3d77e23cc097c3bf6e8054ffce4ef82
kker <907765631@qq.com>
2020-09-01 00:28:26 +0800

    [Mamingjun] feat: complete exercise_04

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,24 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (array === null || array === undefined) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  class Node {
+    constructor(value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+  const head = new Node(array[0]);
+  let p = head;
+  for (let i = 1; i < array.length; i += 1) {
+    const temp = new Node(array[i]);
+    p.next = temp;
+    p = temp;
+  }
+  return head;
 }
