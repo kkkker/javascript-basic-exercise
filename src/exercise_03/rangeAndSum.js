cc45ea078ab7a10154dfdf688d172ca04c020821
kker <907765631@qq.com>
2020-08-31 23:41:10 +0800

    [Mamingjun] feat: complete exercise_03

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,8 +6,18 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const min = Math.min(start, end);
+  const max = Math.max(end, start);
+  const res = [];
+  for (let i = min; i <= max; i += 1) {
+    res.push(i);
+  }
+  if (start < end) {
+    res.pop();
+  } else {
+    res.reverse().pop();
+  }
+  return res;
 }
 
 export function sum(...numbers) {
@@ -18,5 +28,9 @@ export function sum(...numbers) {
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  let res = 0;
+  for (let i = 0; i < numbers.length; i += 1) {
+    res += numbers[i];
+  }
+  return res;
 }
